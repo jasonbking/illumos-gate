4e0571ef09338aca52a7f108044b914160af56ca
Jason King <jason.king@joyent.com>
2020-12-05 16:29:56 -0600

    13962 Support using CPUID instruction for TSC frequency when available
    Change-Id: I42dfad68819c103fcc411cb34dfa2a9452bc2425

--- usr/src/uts/intel/os/cpuid.c
+++ usr/src/uts/intel/os/cpuid.c
@@ -1931,10 +1931,17 @@ struct cpuid_info {
 	 */
 	uint_t cpi_topo_nleaves;
 	struct cpuid_regs cpi_topo[NMAX_CPI_TOPO];
+
+	uint64_t cpi_tsc_freq;		/* TSC frequency in Hz */
 };
 
-
 static struct cpuid_info cpuid_info0;
+
+/*
+ * The 'core crystal clock' frequency of Denverton SoCs in Hz (see
+ * cpuid_basic_freq()).
+ */
+#define	CPI_DENVERTON_CRYSTAL_HZ	25000000
 
 /*
  * These bit fields are defined by the Intel Application Note AP-485
@@ -3995,6 +4002,44 @@ cpuid_basic_ppin(cpu_t *cpu, uchar_t *featureset)
 		break;
 	}
 }
+
+static void
+cpuid_basic_freq(cpu_t *cpu)
+{
+	struct cpuid_info *cpi = cpu->cpu_m.mcpu_cpi;
+	struct cpuid_regs regs;
+	uint64_t crystal, tsc_num, tsc_den;
+
+	cpi->cpi_tsc_freq = 0;
+
+	if (cpi->cpi_vendor != X86_VENDOR_Intel ||
+	    cpi->cpi_maxeax < 0x15)
+		return;
+
+	bzero(&regs, sizeof (regs));
+	regs.cp_eax = 0x15;
+	__cpuid_insn(&regs);
+
+	if (regs.cp_eax == 0 || regs.cp_ebx == 0)
+		return;
+
+	crystal = regs.cp_ecx;
+	tsc_num = regs.cp_ebx;
+	tsc_den = regs.cp_eax;
+
+	/*
+	 * Linux has discovered that Denverton SoCs do not report their
+	 * core crystal clock value (ECX) in leaf 0x15. They do however
+	 * provide the TSC ratio values (EBX/EAX). The core clock crystal
+	 * frequency is a known fixed value (25Mhz), so we use that for
+	 * those systems.
+	 */
+	if (crystal == 0 && cpi->cpi_model == INTC_MODEL_DENVERTON)
+		crystal = CPI_DENVERTON_CRYSTAL_HZ;
+
+	if (crystal != 0)
+		cpi->cpi_tsc_freq = tsc_num * crystal / tsc_den;
+}
 #endif	/* ! __xpv */
 
 static void
@@ -4858,6 +4903,7 @@ cpuid_pass_basic(cpu_t *cpu, void *arg)
 	cpuid_basic_thermal(cpu, featureset);
 #if !defined(__xpv)
 	cpuid_basic_ppin(cpu, featureset);
+	cpuid_basic_freq(cpu);
 #endif
 
 	if (cpi->cpi_vendor == X86_VENDOR_AMD ||
@@ -8630,4 +8676,14 @@ cpuid_getcache(struct cpu *cpu, uint32_t cno, x86_cache_t *cache)
 	cache->xc_id |= (uint64_t)cpi->cpi_apicid >> cache->xc_apic_shift;
 
 	return (0);
+}
+
+/*
+ * Returns the TSC frequency in Hz. If the CPU does not provide the TSC
+ * frequency, the value returned is 0.
+ */
+uint64_t
+cpuid_tsc_freq(cpu_t *cpu)
+{
+	return (cpu->cpu_m.mcpu_cpi->cpi_tsc_freq);
 }
