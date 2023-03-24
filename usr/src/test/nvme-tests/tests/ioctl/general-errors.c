98a0e4f7520b6548c5ae0ec2322e85440668d236
Jason King <jason.brian.king@gmail.com>
2023-03-24 18:47:01 +0000

    NVMe security send/recv support

--- usr/src/test/nvme-tests/tests/ioctl/general-errors.c
+++ usr/src/test/nvme-tests/tests/ioctl/general-errors.c
@@ -39,7 +39,7 @@
 
 #include "nvme_ioctl_util.h"
 
-#if NVME_IOC_MAX != NVME_IOC_SET_FEATURE
+#if NVME_IOC_MAX != NVME_IOC_SECURITY_RECV
 #error	"NVME_IOC_MAX has grown, update this test!"
 #endif
 
@@ -67,7 +67,9 @@ static ioctl_test_t ioctl_tests[NVME_IOC_MAX - NVME_IOC + 1] = {
 	{ NVME_IOC_CTRL_DETACH, true, true },
 	{ NVME_IOC_NS_CREATE, true, true },
 	{ NVME_IOC_NS_DELETE, true, true },
-	{ NVME_IOC_SET_FEATURE, true, true }
+	{ NVME_IOC_SET_FEATURE, true, true },
+	{ NVME_IOC_SECURITY_SEND, true, true },
+	{ NVME_IOC_SECURITY_RECV, true, true },
 };
 
 static bool
